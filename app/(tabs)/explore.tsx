9e5c9c0909ce52d9afe174daa4348cebe58db0c9
Frank Calise <fcalise@gmail.com>
2024-08-23 17:11:03 -0400

    draggable flatlist

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,102 +1,153 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import { ThemedText } from "@/components/ThemedText";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import DraggableFlatList, { OpacityDecorator, RenderItemParams, ScaleDecorator, ShadowDecorator } from 'react-native-draggable-flatlist';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const TASKS = [{
+  id: 1,
+  description: "Task 1",
+  position: 1,
+}, {
+  id: 2,
+  description: "Task 2",
+  position: 2,
+}]
 
-export default function TabTwoScreen() {
+const TaskListItem = ({ task, drag, isActive }) => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <OpacityDecorator activeOpacity={0.9}>
+      <ScaleDecorator activeScale={1.05}>
+    <TouchableOpacity
+      style={[
+        styles.taskItem,
+        { backgroundColor: isActive ? '#2C3E50' : '#1D2125' }
+      ]}
+      onLongPress={drag}
+    >
+      <ThemedText lightColor="white" darkColor="white">{task.description}</ThemedText>
+      <TouchableOpacity onPressIn={drag} style={styles.dragHandle}>
+        <Text style={styles.dragHandleText}>≡</Text>
+      </TouchableOpacity>
+    </TouchableOpacity>
+    </ScaleDecorator>
+    </OpacityDecorator>
+  );
+};
+
+/*
+const TaskListItem = ({ task, drag, isActive }) => {
+  return (
+    <ShadowDecorator>
+      <ScaleDecorator>
+        <OpacityDecorator>
+          <TouchableOpacity
+            activeOpacity={1}
+            onLongPress={drag}
+            disabled={isActive}
+            style={[
+              styles.taskItem,
+              { backgroundColor: isActive ? "blue" : '#1D2125' },
+            ]}
+          >
+            <ThemedText lightColor="white" darkColor="white">{task.description}</ThemedText>
+          </TouchableOpacity>
+        </OpacityDecorator>
+      </ScaleDecorator>
+    </ShadowDecorator>
+  );
+};*/
+
+export default function TaskList() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [newTask, setNewTask] = useState("");
+
+
+  const createTask = () => {
+    const id = tasks.length + 1;
+
+    setTasks([...tasks, {
+      id ,
+      description: newTask,
+      position: tasks.length + 1,
+    }]);
+    setNewTask("");
+  };
+
+  const renderItem = ({ item, drag, isActive }: RenderItemParams<typeof item>) => (
+    <TaskListItem task={item} drag={drag} isActive={isActive} />
+  );
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="light" />
+        <Text style={styles.title}>Todo</Text>
+
+        <DraggableFlatList
+          data={tasks}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          onDragEnd={({ data }) => setTasks(data)}
+        />
+
+        {/* New task input */}
+        <TextInput
+          value={newTask}
+          onChangeText={setNewTask}
+          placeholder="New task"
+          placeholderTextColor="gray"
+          style={styles.input}
+          onSubmitEditing={createTask}
+        />
+        <Button title="Add task" onPress={createTask} />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    backgroundColor: "#101112",
+    padding: 10,
+    borderRadius: 5,
+    gap: 5,
+    flex: 1,
   },
-  titleContainer: {
+  title: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  input: {
+    color: "white",
+    padding: 15,
+    backgroundColor: "#1D2125",
+    borderRadius: 5,
+  },
+  taskItem: {
     flexDirection: 'row',
-    gap: 8,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FFF",
+  },
+  dragHandle: {
+    padding: 10,
+  },
+  dragHandleText: {
+    color: 'white',
+    fontSize: 24,
   },
 });
